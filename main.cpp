356f744dc159fbe8299dc580cad19ef6f0712de5
Randy <randomize46@gmail.com>
2015-07-25 13:53:01 +0300

    OpenEXR tested and works

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 
 
-#include "tinyexr/tinyexr.h"
+// #include "tinyexr/tinyexr.h"
 
 #include <string>
 #include <vector>
@@ -8,10 +8,12 @@
 #include <cstdio>
 #include <cmath>
 
+#include <ImfRgbaFile.h>
+
 namespace Patterns {
-const char *InputColorPattern = "out.VRayZDepth.%04d.exr";
-const char *InputDepthPattern = "out.VRayZDepth.%04d.exr";
-const size_t MAX_PATTERN_LEN = 255;
+    const char *InputColorPattern = "out.VRayZDepth.%04d.exr";
+    const char *InputDepthPattern = "out.VRayZDepth.%04d.exr";
+    const size_t MAX_PATTERN_LEN = 255;
 }
 
 void LeadZeros() {}
@@ -19,36 +21,49 @@ void LeadZeros() {}
 void LoadImage(const char *pattern, size_t index) {
 
     // Form a name
-    static char filaname[Patterns::MAX_PATTERN_LEN];
-    snprintf(filaname, Patterns::MAX_PATTERN_LEN, pattern, index);
+    static char fileName[Patterns::MAX_PATTERN_LEN];
+    snprintf(fileName, Patterns::MAX_PATTERN_LEN, pattern, index);
 
-    // Load image
-    float *out; // width * height * RGBA
-    int width;
-    int height;
-    const char *err;
+    std::vector<Imf::Rgba> pixelBuffer;
 
-    int ret = LoadEXR(&out, &width, &height, filaname, &err);
 
-    if (ret != 0) {
-        printf("Error loading EXR from \"%s\" code=%d\n and err=%s", filaname, ret, err);
+    try
+    {
+        Imf::RgbaInputFile in(fileName);
+
+
+        Imath::Box2i win = in.dataWindow();
+
+        Imath::V2i dim(win.max.x - win.min.x + 1,
+                win.max.y - win.min.y + 1);
+
+        printf("Loaded EXR \"%s\", image has %dx%d ", fileName, dim.x, dim.y);
+
+        pixelBuffer.resize(dim.x * dim.y);
+
+        int dx = win.min.x;
+        int dy = win.min.y;
+
+        in.setFrameBuffer(pixelBuffer.data() - dx - dy * dim.x, 1, dim.x);
+        in.readPixels(win.min.y, win.max.y);
+    }
+    catch(Iex::BaseExc & e)
+    {
+        printf("Error loading EXR from \"%s\" err=%s", fileName, e.what());
         return;
     }
 
-    printf("Loaded EXR \"%s\", image has %dx%d ", filaname, width, height);
-    
 
-    delete[] out;
 }
 
 int main(int argc, char *argv[]) { 
 
     printf("Randy depth composer utility");
     printf("Usage : dcompose <number of frames>");
-    
+
 
     LoadImage( Patterns::InputDepthPattern, 1 );
-    
+
     return 0;
 
 }
